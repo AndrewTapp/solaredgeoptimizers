c1c7685b5715af7241d8a0a80998b88baf13c5c3
AndrewTapp <83402277+AndrewTapp@users.noreply.github.com>
2026-01-29 10:56:55 +0000

    Update docstring for config flow module

--- custom_components/solaredgeoptimizers/config_flow.py
+++ custom_components/solaredgeoptimizers/config_flow.py
@@ -1,4 +1,4 @@
-"""Config flow for SolarEdge Optimizers Data integration."""
+"""Configuration flow for SolarEdge Optimizers Home Assistant integration."""
 from __future__ import annotations
 
 import logging
